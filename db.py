d3ab0e1d58d386dfcaa1b0c16960a1a71dd094bc
tvrocha <tuliovcr@outlook.com>
2024-09-11 10:12:48 -0300

    feat: gitignore

--- db.py
+++ db.py
@@ -14,6 +14,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
+
 # Função para salvar os dados validados no PostgreSQL
 def salvar_no_postgres(dados: Vendas):
     try:
